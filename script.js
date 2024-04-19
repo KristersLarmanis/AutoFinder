e0c6b7b00d1fa91851cf3fad0871d0a49f1c84ce
kristers Lrmanis <[kristers.larmanis@svs.edu.lv]>
2024-04-19 12:35:43 +0300

    Initial

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const model = document.getElementById("model");
-document.getElementById("poga").addEventListener("click",function(){
-    
+const carYear = document.getElementById("carYear");
+const maxPrice = document.getElementById("maxPrice");
+const minPrice = document.getElementById("minPrice");
+document.getElementById("poga").addEventListener("click", function(){
+const modelaVertiba = document.getElementById("model").value.trim(); // trim() noņem tukšumus no sākuma un beigām
+if (modelaVertiba === "") {
+    alert("Lūdzu, ievadiet mašīnas modeli.");
+    return; // Iziet no funkcijas, ja modelis nav ievadīts
+}
+const URL = "https://www.ss.com/lv/transport/cars/" + modelaVertiba + "/rss/";
+// Tagad varat izmantot URL turpmākai apstrādei
 });
-
-const URL = "https://www.ss.com/lv/transport/cars/"+model+"/rss/";
