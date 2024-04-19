acef4f3434aef770d25eb553da2a4f610fd95fc6
kristers Lrmanis <[kristers.larmanis@svs.edu.lv]>
2024-04-19 11:54:30 +0300

    Initial

---
+++ script.js
@@ -0,0 +1,6 @@
+const model = document.getElementById("model");
+document.getElementById("poga").addEventListener("click",function(){
+    
+});
+
+const URL = "https://www.ss.com/lv/transport/cars/"+model+"/rss/";
