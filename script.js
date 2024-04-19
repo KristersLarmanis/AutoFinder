2ab4387ff67aee01bc5e00382feaa5fca54abd17
Regnars Tamms <166803742+regnarstamms@users.noreply.github.com>
2024-04-19 12:59:49 +0300

    Initial

--- script.js
+++
@@ -1,6 +0,0 @@
-const model = document.getElementById("model");
-document.getElementById("poga").addEventListener("click",function(){
-    
-});
-
-const URL = "https://www.ss.com/lv/transport/cars/"+model+"/rss/";
